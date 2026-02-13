77bd52e871c1fb5f888429ce309b9c6fbac33c42
duviolin <eduardolima384@gmail.com>
2026-02-13 10:28:39 -0300

    add docs

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Limpa todo o cache do navegador a cada refresh (F5)
+localStorage.clear();
+
 createRoot(document.getElementById("root")!).render(<App />);
